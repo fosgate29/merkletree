ccc7ff086905a3760e72bb8df92068c7eeb9378f
Luiz Soares <luizhamilton29@gmail.com>
2023-05-11 21:04:17 -0300

    Added code using OPZ lib

--- contracts/MTbatch.sol
+++ contracts/MTbatch.sol
@@ -1,70 +1,24 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MTbatch is ERC20, Ownable {
+contract Verifier {
+    bytes32 private root;
 
-    /// @notice Merkle proof of token amounts assigned to different addresses
-    bytes32 root;
-    
-    constructor() ERC20("Token", "TKN"){
-        _mint(address(this), 10 * 2**8);
-    }
-
-    function executeTransfer(bytes32[] calldata _merkleProof,  address[] calldata _users, uint256[] calldata _amounts) external  {
-        
-        require(_users.length > 0, "Size cant be zero");
-        require(_users.length == _amounts.length, "Different array size");
-
-        bytes32 finalHash = hashArray(_users, _amounts);
-
-        require(canTransfer(finalHash, _merkleProof), "Invalid leave");
-
-        address from = address(this);
-
-        for(uint256 i=0; i<_users.length ; i++){
-            _transfer(from, _users[i], _amounts[i]);
-        }
-    }
-
-    /// @notice Merkle proof of token amounts assigned to different addresses
-    function setProof(bytes32 _root) external onlyOwner {
+    constructor(bytes32 _root) {
+        // (1)
         root = _root;
     }
 
-    function canTransfer(bytes32 finalHash, bytes32[] calldata _merkleProof)
-        internal
-        view
-        returns (bool)
-    {
-        return MerkleProof.verify(_merkleProof, root, finalHash);
+    function verify(
+        bytes32[] memory proof,
+        address addr,
+        uint256 amount
+    ) public {
+        // (2)
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(addr, amount))));
+        // (3)
+        require(MerkleProof.verify(proof, root, leaf), "Invalid proof");
+        
     }
-
-    function hashArray(address[] calldata _users, uint256[] calldata _amounts)
-        public
-        pure
-        returns (bytes32 finalHash)
-    {
-
-
-        finalHash = keccak256(abi.encodePacked(_users[0], _amounts[0]));
-
-        if(_users.length > 1){
-            for(uint256 i=1; i< _users.length; i++){
-                finalHash = keccak256(abi.encodePacked(finalHash, _users[i], _amounts[i]));
-            }
-        }
-    }
-
-    function abiencoded(address[] calldata accounts, uint256[] calldata amounts) external view virtual returns (bytes memory) {
-       return abi.encodePacked(accounts, amounts);
-	}
-
-	function getLeaf(address account, uint256 amount) external view virtual returns (bytes32 ) {
-       bytes32 leaf = keccak256(abi.encodePacked(account, amount));
-       return leaf;
-	}
 }
--- scripts/testop.js
+++ scripts/testop.js
@@ -1,12 +1,64 @@
 const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
+const keccak256 = require('keccak256');
+const ethers = require('ethers');
 
-const values = [
-  ['0x1111111111111111111111111111111111111111', '5000000000000000000'],
-  ['0x2222222222222222222222222222222222222222', '2500000000000000000']
-];
+let hashes = [];
+
+// hash each transfer tx
+let finalHash;
+
+const values = [];
+
+finalHash = keccak256(
+  '0x5b38da6a701c568545dcfcb03fcb875f56beddc400000000000000000000000000000000000000000000000000000000000001f4'
+);
+
+let tupla = ethers.utils.solidityPack(
+  ['bytes32', 'address', 'uint256'],
+  [finalHash, '0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2', 500]
+);
+
+finalHash = keccak256(tupla);
+
+hashes.push('0x' + finalHash.toString('hex'));
+values.push(hashes);
+
+for (let i = 1; i < 4; i++) {
+  let newAddress = ethers.Wallet.createRandom().address;
+  let amount = i;
+  tupla = ethers.utils.solidityPack(['address', 'uint256'], [newAddress, amount]);
+  finalHash = keccak256(tupla);
+
+  for (let j = 1; j < 7; j++) {
+    newAddress = ethers.Wallet.createRandom().address;
+    amount = i + j;
+    tupla = ethers.utils.solidityPack(['bytes32', 'address', 'uint256'], [finalHash, newAddress, amount]);
+    finalHash = keccak256(tupla);
+  }
+
+  hashes = [];
+  hashes.push('0x' + finalHash.toString('hex'));
+  values.push(hashes);
+}
+
+// create tree
+
+// ['0xbc4b8c6cc50f1918f2c33031c8b93ea7b400500474068e5bec1dba708e3f8ef6'],
+// ['0x17a9a1c28112f9d1a9234400cdeff0fcc07363f50bcb3940f0b36ea198666c2e']
+//];
+console.log(values);
 
 // (2)
-const tree = StandardMerkleTree.of(values, ['address', 'uint256']);
+const tree = StandardMerkleTree.of(values, ['bytes32']);
 
 // (3)
 console.log('Merkle Root:', tree.root);
+
+for (const [i, v] of tree.entries()) {
+  if (v[0] === '0xbc4b8c6cc50f1918f2c33031c8b93ea7b400500474068e5bec1dba708e3f8ef6') {
+    // (3)
+    const proof = tree.getProof(i);
+    console.log('Value:', v);
+    console.log('Proof:', proof);
+  }
+}
